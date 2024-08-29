6d4317b7dd7346be01090cb37218a1e591770253
moonachi <blogkin@naver.com>
2024-08-29 17:25:04 +0900

    asd

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          <code className={styles.code}>app/page.tsx</code>
+          <code className={styles.code}>app/.tsx</code>
         </p>
         <div>
           <a

539eceff78d0fa0d3d2ca710a7284f7f12e52f4e
moonachi <blogkin@naver.com>
2024-08-29 17:31:59 +0900

    dev

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          <code className={styles.code}>app.tsx</code>
+          <code className={styles.code}>app.x</code>
         </p>
         <div>
           <a

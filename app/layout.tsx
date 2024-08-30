0deeef01d2aabc0f333ca4bcab66db4e0bbb9016
moonachi <blogkin@naver.com>
2024-08-30 11:06:37 +0900

    asdf

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {};
-const unusedVariable = "This variable is not used";
 
 export default function RootLayout({
   children

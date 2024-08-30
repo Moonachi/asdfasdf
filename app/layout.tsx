3fd09cbaabba078bc02436a5f05c848de8668fbb
moonachi <blogkin@naver.com>
2024-08-30 11:08:24 +0900

    asd

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {};
+const unusedVariable = "This variable is not used";
 
 export default function RootLayout({
   children
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-console.log();
+cosole.log();
 
 export default function Home() {
   return (
